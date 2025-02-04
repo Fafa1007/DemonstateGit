8364fa025525741473b6bce5cec399889981d34b
ESNCHR001 <ESNCHR001@myuct.ac.za>
2025-02-04 23:02:17 +0200

    set up git hub

--- DemGit.R
+++ DemGit.R
@@ -1,2 +1,11 @@
 install.packages("usethis")
+
+# set your username and your email address
+username <- "ESNCHR001"
+email <- "ESNCHR001@myuct.ac.za" # replace NULL 
+stopifnot(!is.null(username)) # just checking you set this
+stopifnot(!is.null(email))
+gert::git_config_global_set("user.name",  username)
+gert::git_config_global_set("user.email", email)
+
 usethis::use_git()
